192531c8ba07b707325552a079067262854396d2
22520 <jonathon.frisco@vtsd.com>
2024-10-08 13:44:43 -0400

    Solution to step 18 with new colors (Orange and Black)

--- 1-1-7/the_step_nine.py
+++ 1-1-7/the_step_nine.py
@@ -1,18 +1,22 @@
 #   a117_traversing_turtles.py
 #   Add code to make turtles move in a circle and change colors.
 import turtle as trtl
+from typing import List
 
 # create an empty list of turtles
 my_turtles = []
 
 index = 0
+direction = 90
 # use interesting shapes and colors
 turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle", "classic"]
-turtle_colors = ["red", "blue", "green", "orange", "purple", "gold"]
+turtle_colors: list[str] = ["red", "blue", "green", "orange", "purple", "gold"]
 
 for s in turtle_shapes:
   t = trtl.Turtle(shape=s)
   t.color(turtle_colors.pop())
+
+
   index += 1
   t.penup()
   my_turtles.append(t)
@@ -24,10 +28,13 @@ starty = 0
 #
 for t in my_turtles:
   t.goto(startx, starty)
+  t.setheading(direction)
   t.pendown()
   t.right(45)
   t.forward(50)
-  new_color = turtle_colors.pop()
+  direction = t.heading()
+
+
 #
   startx = t.xcor()
   starty = t.ycor()
