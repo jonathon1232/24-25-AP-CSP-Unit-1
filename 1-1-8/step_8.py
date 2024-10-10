f44f25f1e6873f834d9bdf265dd5a1f488ed2685
22520 <jonathon.frisco@vtsd.com>
2024-10-10 12:25:37 -0400

    Solution to step 18 with new colors (Orange and Black)

---
+++ 1-1-8/step_8.py
@@ -0,0 +1,11 @@
+num = int(input("Enter a number -> "))
+if (69 >= num >= 60):
+  print("Input is 60 or more")
+elif (79 >= num >= 70):
+  print("Input is 70 or more")
+elif (89 >= num >= 80):
+  print("Input is 80 or more")
+elif (num >= 90):
+  print("Input is 90 or more")
+else:
+  print("Input is less than 60")
